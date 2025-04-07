d5d83a158e0b6348d3e8c506199e9f7edb0b303b
MariusVinclair <marius.vinclair@gmail.com>
2025-04-07 08:40:56 +0000

    5000

---
+++ tp4/src/main/java/fr/ensai/running/model/Competition.java
@@ -0,0 +1,26 @@
+package fr.ensai.running.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+import java.time.LocalDate;
+
+@Entity
+@Getter
+@Setter
+public class Competition {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id_competition")
+    private long idCompetition;
+
+    private String designation;
+    private String city;
+    private LocalDate eventDate;
+    private Float distance;
+    private int maxAthletes;
+}
