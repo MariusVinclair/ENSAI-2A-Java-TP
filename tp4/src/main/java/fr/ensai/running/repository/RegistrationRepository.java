eacc2ce237a6db26a74a8489be291cbd00a2e1c3
MariusVinclair <marius.vinclair@gmail.com>
2025-04-07 09:11:57 +0000

    5000

--- tp4/src/main/java/fr/ensai/running/repository/RegistrationRepository.java
+++ tp4/src/main/java/fr/ensai/running/repository/RegistrationRepository.java
@@ -1,10 +1,21 @@
 package fr.ensai.running.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import fr.ensai.running.model.Athlete;
 import fr.ensai.running.model.Registration;
+import java.util.List;
 
 public interface RegistrationRepository extends JpaRepository<Registration, Long> {
 
     Registration findByIdAthleteAndIdCompetition(long idAthlete, long idCompetition);
+
+    @Query(value = """
+            SELECT r.athlete.id
+            FROM Registration r
+            WHERE r.competition.id = :idCompetition
+            """, nativeQuery = false)
+    List<Athlete> findAthleteIdByCompetitionId(@Param("idCompetition") Long idCompetition);
 }
