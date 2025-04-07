25d329d48f99bdd46fedab01c0f11b159630376b
MariusVinclair <marius.vinclair@gmail.com>
2025-04-07 08:25:49 +0000

    debut tp4

--- tp4/src/main/java/fr/ensai/running/controller/ApiRestController.java
+++ tp4/src/main/java/fr/ensai/running/controller/ApiRestController.java
@@ -3,7 +3,13 @@ package fr.ensai.running.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,6 +30,40 @@ public class ApiRestController {
     public List<Athlete> allAthletes() {
 
         return athleteService.findAll();
+    }
+
+    /**
+     * Get an Athlete by id
+     */
+    @GetMapping("/athlete/{id}")
+    public ResponseEntity<Athlete> getAthleteById(@PathVariable Long id) {
+        Athlete athlete = athleteService.findById(id);
+        if (athlete == null) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(athlete);
+    }
+
+    /**
+     * Delete an Athlete by id
+     */
+    @DeleteMapping("/athlete/{id}")
+    public ResponseEntity<Void> deleteAthleteById(@PathVariable Long id) {
+        Athlete existingAthlete = athleteService.findById(id);
+        if (existingAthlete == null) {
+            return ResponseEntity.notFound().build();
+        }
+        athleteService.deleteById(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    /**
+     * Create an Athlete
+     */
+    @PostMapping("/athlete")
+    public ResponseEntity<Athlete> createUser(@RequestBody Athlete athlete) {
+        Athlete createdAthlete = athleteService.save(athlete);
+        return ResponseEntity.status(HttpStatus.CREATED).body(createdAthlete);
     }
 
 }
--- tp4/src/main/java/fr/ensai/running/controller/AthleteController.java
+++ tp4/src/main/java/fr/ensai/running/controller/AthleteController.java
@@ -1,6 +1,7 @@
 package fr.ensai.running.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
